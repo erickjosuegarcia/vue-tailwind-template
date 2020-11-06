8492bcef2676cd80a80024bd1a34411dbc7f9905
Erick García <erick.josue.garcia@gmail.com>
2020-11-06 11:10:28 -0600

    Vue + Tailwind Scaffolding
    Template básico de una aplicación Vue con Tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Navigation from './components/Navigation.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+//Register global components
+app.component('Navigation', Navigation)
+
+//Mount application
+app.use(router).mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="about">
+	<header class="bg-white shadow">
+		<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+			<h1 class="text-3xl font-bold leading-tight text-gray-900">
+			About
+			</h1>
+		</div>
+	</header>
+	<main>
+		<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+			<div class="md:flex bg-white rounded-lg p-6">
+				<img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="../assets/avatar.jpg">
+				<div class="text-center md:text-left">
+					<h2 class="text-lg">Erin Lindford</h2>
+					<div class="text-purple-500">Product Engineer</div>
+					<div class="text-gray-600">erinlindford@example.com</div>
+					<div class="text-gray-600">(555) 765-4321</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
+</template>
